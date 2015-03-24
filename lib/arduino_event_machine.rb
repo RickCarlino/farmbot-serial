1f8b08a9e756cb53bcb8155f8665b8253cdf6b91
Rick Carlino <rick.carlino@gmail.com>
2015-03-24 16:12:37 -0500

    Add polling

--- lib/arduino_event_machine.rb
+++ lib/arduino_event_machine.rb
@@ -7,6 +7,26 @@ module FB
       attr_accessor :arduino
     end
 
+    def self.poll(interval, &blk)
+      raise 'You must pass a block' unless block_given?
+      @polling_interval = interval.to_f
+      @polling_callback = blk
+    end
+
+    # TODO: Not sure if its a good idea to be calling EM in here if we ever want
+    # to add other EM based services. Will leave for now.
+    def self.start(arduino)
+      @arduino = arduino
+      EM.run do
+        EM.attach arduino.serial_port, self
+        if @polling_callback
+          EventMachine::PeriodicTimer.new(@polling_interval) do
+            @polling_callback.call(@arduino)
+          end
+        end
+      end
+    end
+
     def receive_data(data)
       self.class.arduino.read(data)
     end
--- lib/default_serial_port.rb
+++ lib/default_serial_port.rb
@@ -2,18 +2,19 @@ require 'serialport'
 
 # This object creates a Serial IO with sane default, since most FarmBot setups
 # follow the same serial configuration setup.
-class DefaultSerialPort < SerialPort
-  COM_PORT = '/dev/ttyACM0'
-  OPTIONS  = { "baud"         => 115200,
-               "data_bits"    => 8,
-               "stop_bits"    => 1,
-               "parity"       => SerialPort::NONE,
-               "flow_control" => SerialPort::SOFT }
+module FB
+  class DefaultSerialPort < SerialPort
+    COM_PORT = '/dev/ttyACM0'
+    OPTIONS  = { "baud"         => 115200,
+                 "data_bits"    => 8,
+                 "stop_bits"    => 1,
+                 "parity"       => SerialPort::NONE,
+                 "flow_control" => SerialPort::SOFT }
 
-  # Why `def self::new()`? it was defined that way in the parent class,
-  # therefore, I can't call super in #initialize().
-  def self::new(com = COM_PORT, conf = OPTIONS)
-    super
+    # Why `def self::new()`? it was defined that way in the parent class,
+    # therefore, I can't call super in #initialize().
+    def self::new(com = COM_PORT, conf = OPTIONS)
+      super
+    end
   end
 end
-
