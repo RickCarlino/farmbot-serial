8ddf93636a5d42186a80abada8be9e8a55c65ce5
Rick Carlino <rick.carlino@gmail.com>
2015-03-27 15:53:56 -0500

    Show Tim status idea

--- lib/arduino/status.rb
+++ lib/arduino/status.rb
@@ -3,7 +3,7 @@ module FB
     # Map of informational status and default values for status within Arduino.
     DEFAULT_INFO = {X: 0, Y: 0, Z: 0, S: 10, Q: 0,  T: 0,  C: '', P: 0,  V: 0,
                     W: 0, L: 0, E: 0, M: 0, XA: 0, XB: 0, YA: 0, YB: 0, ZA: 0,
-                   ZB: 0}
+                   ZB: 0, busy: 1}
     # Put it into a struct.
     Info = Struct.new(*DEFAULT_INFO.keys)
 
@@ -13,9 +13,16 @@ module FB
       @bot, @info = bot, Info.new(*DEFAULT_INFO.values)
     end
 
-    def parse_incoming(gcode)
-      puts 'Not implemented yet. Publish a `change` event here'
-      # React to incoming serial changes here.
+    def parse_incoming(gcode) # gcode is an actual Gcode object, not String.
+      # This method will always be the first in line to send a bot a message
+      # and control traffic. Default business status is 1.
+      case gcode.status_effect
+      when :done;     self[:busy] = 0
+      when :received; self[:busy] = 1
+      when :busy;     self[:busy] = 1
+      when :error;    raise HardwareError # We should treat errors like errors.
+      else;           business_as_usual   # Not a real method. Could be, though.
+      end
     end
 
     def []=(register, value)
--- lib/gcode.rb
+++ lib/gcode.rb
@@ -13,13 +13,29 @@ module FB
       string.gsub("\r", '').split("\n").map { |s| self.new(s) }
     end
 
+    # Determine if the current Gcode is one that affects the bot's availability
+    # status.
+    def status_changer?
+      status_effect != :none
+    end
+
+    def status_effect
+      case (@cmd.head == :R) && cmd.tail.to_i
+      when 1; :received
+      when 2; :done
+      when 3; :error
+      when 4; :busy
+      else;   :none
+      end
+    end
+
     # Seperate head of Params / Gcode from the 'tails'
     class GcodeToken
       attr_reader :head, :tail, :name
 
       def initialize(str)
         nodes = str.scan(/\d+|\D+/) # ["R", "01"]
-        @head, @tail = nodes.shift.to_sym, nodes
+        @head, @tail = nodes.shift.to_sym, nodes.join(" ")
       end
     end
   end
