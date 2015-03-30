4bd1bc21b2d43c9b9b9cf478c0e08fc1a512f418
Rick Carlino <rick.carlino@gmail.com>
2015-03-30 14:34:32 -0500

    patch Struct#to_h to support old rubies

---
+++ spec/lib/arduino_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+describe FB::Arduino do
+  it("works"){ expect(true).to be_truthy }
+end
+
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,11 +7,4 @@ require 'pry'
 require 'farmbot-serial'
 
 RSpec.configure do |config|
-  config.expect_with :rspec do |expectations|
-    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
-  end
-
-  config.mock_with :rspec do |mocks|
-    mocks.verify_partial_doubles = true
-  end
 end
