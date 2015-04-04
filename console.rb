525fb3e777b4cd1fa6a810a88367ba91acfceea9
Rick Carlino <rick.carlino@gmail.com>
2015-04-04 13:37:55 -0500

    Added console

---
+++ console.rb
@@ -0,0 +1,51 @@
+require_relative 'lib/farmbot-serial'
+require 'pry'
+
+bot = FB::Arduino.new # Defaults to '/dev/ttyACM0', can be configured.
+
+puts """
+FARMBOT SERIAL SANDBOX. WELCOME!
+================================
+Example commands:
+emergency_stop
+move_relative x: 600, y: 100, z: 4
+move_relative y: -600
+home_x
+home_y
+home_z
+home_all
+read_parameter(8)
+write_parameter('x', 0)
+write_pin(pin: 8, value: 1, mode: 1)
+read_status(8)
+"""
+print "> "
+
+class KeyboardHandler < EM::Connection
+  include EM::Protocols::LineText2
+
+  attr_reader :bot
+
+  def initialize(bot)
+    @bot = bot
+  end
+
+  def receive_line(data)
+    puts "=>", bot.commands.instance_eval(data)
+    print "> "
+  rescue Exception => exc
+    exit(0) if data.start_with?('q')
+    puts "#{exc.message} : "
+    print "> "
+  end
+end
+
+EM.run do
+  FB::ArduinoEventMachine.connect(bot)
+  bot.onmessage { |gcode| puts "INCOMING GCODE: #{gcode.name}; " }
+  bot.onchange { |diff| puts " STATUS CHANGE: #{diff}; " }
+  bot.onclose { puts "bye!"; EM.stop } # Unplug the bot and see
+  # EventMachine::PeriodicTimer.new(2) { bot.serial_port.puts "G82" }
+  EM.open_keyboard(KeyboardHandler, bot)
+end
+
