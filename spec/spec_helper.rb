4aa0922b83a2429c3725977a5fc964ef4f11ad8c
Rick Carlino <rick.carlino@gmail.com>
2015-05-05 09:24:00 -0500

    Added ruby-prof to spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,7 @@ require 'simplecov'
 SimpleCov.start do
   add_filter "/spec/"
 end
+
 require 'pry'
 require 'farmbot-serial'
 
@@ -10,7 +11,20 @@ require_relative 'fakes/fake_serial_port'
 require_relative 'fakes/fake_logger'
 require_relative 'fakes/fake_arduino'
 
+require 'ruby-prof'
+
 RSpec.configure do |config|
+  config.before(:suite) do
+    # Profile the code
+    RubyProf.start
+  end
+
+  config.after(:suite) do
+    result = RubyProf.stop
+    # Print a flat profile to text
+    printer = RubyProf::FlatPrinter.new(result)
+    printer.print(STDOUT)
+  end
 end
 
 # This is used for testing things that require an event loop. Once run, you can
